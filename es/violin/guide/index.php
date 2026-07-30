c5132bf63785bc836fb31664b9bcf7ee7b816bd1
mpas585 <mpas585@gmail.com>
2026-07-30 22:36:06 +0900

    Delete es directory

--- es/violin/guide/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-  /es/violin/guide/ — お悩み集（記事一覧）。言語と楽器を指定して基幹PHPを呼ぶだけ。
-  画面の中身は includes/views/guide.php、文言は includes/lang/es.php の 'guide'。
-*/
-$LANG       = 'es';
-$INSTRUMENT = 'violin';
-require __DIR__ . '/../../../includes/string_guide.php';
